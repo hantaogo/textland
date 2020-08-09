1e888d13be74eb42796ff5d58b7243ca8d508310
Japple <448248521@qq.com>
2020-08-09 19:27:16 +0800

    为index.js添加注释

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import Game from "@/Core/Game";
 import MainMenu from "@/UI/MainMenu";
-import { weathers, names } from '@/Util';
 
+// 初始化PIXI
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
+// 为了方便调用，app设置为全局变量
 window.app = app;
+
+// 游戏对象是管理游戏舞台的
 const game = new Game();
+// 为了方便调用，app设置为全局变量
 window.game = game;
+
+// 进入主菜单
 game.push(new MainMenu());
-console.log(weathers);
